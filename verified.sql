945da8ad733b5f63cc94dabb8e35f878acc6772d
Verified Properties <75906791+verifiedproperties@users.noreply.github.com>
2022-04-27 19:53:18 -0400

    Added "access_code" column to "work-orders" table.

--- verified.sql
+++ verified.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost:8889
--- Generation Time: Apr 27, 2022 at 11:30 PM
+-- Generation Time: Apr 27, 2022 at 11:51 PM
 -- Server version: 5.7.34
 -- PHP Version: 8.0.8
 
@@ -58,7 +58,7 @@ CREATE TABLE `users` (
 --
 
 INSERT INTO `users` (`id`, `first_name`, `last_name`, `email`, `phone`, `dob`, `password`, `joined`, `status`, `role`) VALUES
-(1, 'Richard', 'Rodgers', 'richard@verifiedproperties.com', NULL, NULL, '$2y$10$u3BwdIbyVkg.vLoghHXuFONmtPsn4MyJHXX6/Xqv3IjgYk/B0SE5m', '2022-04-27 18:21:10', 'active', '2');
+(1, 'Richard', 'Rodgers', 'richard@verifiedproperties.com', NULL, NULL, '$2y$10$u3BwdIbyVkg.vLoghHXuFONmtPsn4MyJHXX6/Xqv3IjgYk/B0SE5m', '2022-04-27 18:21:10', 'active', '1');
 
 -- --------------------------------------------------------
 
@@ -87,8 +87,16 @@ CREATE TABLE `work-orders` (
   `assignee` int(11) DEFAULT NULL,
   `approved_by` varchar(255) DEFAULT NULL,
   `date_completed` datetime DEFAULT NULL,
-  `date_approved` datetime DEFAULT NULL
+  `date_approved` datetime DEFAULT NULL,
+  `access_code` varchar(55) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Dumping data for table `work-orders`
+--
+
+INSERT INTO `work-orders` (`id`, `client_name`, `con`, `street_address`, `secondary_address`, `city`, `state`, `zip`, `county`, `country`, `owner`, `start_date`, `due_date`, `instructions`, `status`, `service`, `date_created`, `assignee`, `approved_by`, `date_completed`, `date_approved`, `access_code`) VALUES
+(1, 'Assero', '1231231232', '2105 Central Avenue', '', 'Middletown', 'Ohio', '45044', 'Butler', 'United States', 'Jameson Rodgers', '2022-04-27 00:00:00', '2022-04-30', '', 'open', 'Interior/Exterior', '2022-04-27 19:50:31', NULL, NULL, NULL, NULL, '12345566');
 
 --
 -- Indexes for dumped tables
@@ -134,7 +142,7 @@ ALTER TABLE `users`
 -- AUTO_INCREMENT for table `work-orders`
 --
 ALTER TABLE `work-orders`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
