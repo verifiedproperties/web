a17af5eb3d45d6f4777e74312773a8f9cf378d28
2020 <dev.expertweblancer@gmail.com>
2022-04-29 17:50:02 +0300

    jwt authentication

---
+++ vendor/composer/installed.php
@@ -0,0 +1,33 @@
+<?php return array (
+  'root' => 
+  array (
+    'pretty_version' => 'dev-main',
+    'version' => 'dev-main',
+    'aliases' => 
+    array (
+    ),
+    'reference' => 'dd76f47f78b5e8f3b7dc127b86138bfc1b537dbd',
+    'name' => '__root__',
+  ),
+  'versions' => 
+  array (
+    '__root__' => 
+    array (
+      'pretty_version' => 'dev-main',
+      'version' => 'dev-main',
+      'aliases' => 
+      array (
+      ),
+      'reference' => 'dd76f47f78b5e8f3b7dc127b86138bfc1b537dbd',
+    ),
+    'firebase/php-jwt' => 
+    array (
+      'pretty_version' => 'v6.1.2',
+      'version' => '6.1.2.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => 'c297139da7c6873dbd67cbd1093f09ec0bbd0c50',
+    ),
+  ),
+);
