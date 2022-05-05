25d39b5e9fc81b300c90aefe05991411ac4cab25
Verified Properties <75906791+verifiedproperties@users.noreply.github.com>
2022-05-05 11:23:38 -0400

    More tests

--- admin/test.php
+++ admin/test.php
@@ -8,5 +8,19 @@ session_start();
 if (!isset($_SESSION['username'])) {
   header('Location: ../login');
 }
+include 'template/head.php';
 
-echo "Hello World!";
+if (isset($_POST['submitt'])) {
+  $fname = $_POST['fname'];
+
+  echo "Yur name is '".$fnam."'";
+  exit();
+}
+
+?>
+
+<form method="post">
+  <label class="form-label">Test</label>
+  <input type="text" name="fname" class="form-control">
+  <button type="submit" name="submitt">Submit</button>
+</form>
