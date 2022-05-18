1304d2a5e6dcb245427316a227aa2b7b24c56327
Verified Properties <75906791+verifiedproperties@users.noreply.github.com>
2022-05-18 12:10:31 -0400

    Test

--- admin/new-order.php
+++ admin/new-order.php
@@ -91,7 +91,8 @@ if (isset($_POST['create-order'])) {
       ob_end_flush();
     }
   } else {
-    $_SESSION['emptyFields'] = "<div class='alert alert-warning text-white'>One or more required fields are empty, please try again!</div>";
+    //$_SESSION['emptyFields'] = "<div class='alert alert-warning text-white'>One or more required fields are empty, please try again!</div>";
+    $query_error = "Failed to create order: " . mysqli_error($conn);
   }
 }
 ?>
