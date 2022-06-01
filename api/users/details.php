022c1fc2605015e9b355a3ce0fed9f60f09c576b
Verified Properties <75906791+verifiedproperties@users.noreply.github.com>
2022-06-01 09:26:17 -0400

    Added id to user details api.

--- api/users/details.php
+++ api/users/details.php
@@ -12,25 +12,25 @@
     header("Access-Control-Allow-Methods: *");
     header("Access-Control-Max-Age: 3600");
     header("Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With");
-    
+
     $database = new Database();
     $db = $database->getConnection();
- 
+
     $id = isset($_GET['user_id']) ? $_GET['user_id'] : die();;
-   
+
     $headers = getallheaders();
     if(array_key_exists("Authorization",$headers)){
         $arr = explode(" ", $headers["Authorization"]);
         $jwt = $arr[1];
-        
+
         if($jwt){
             try {
                 $decoded = JWT::decode($jwt, new Key($secret_key, 'HS256'));
                 $user_id = json_encode($decoded->data->id);
-                // Access is granted. Add code of the operation here 
+                // Access is granted. Add code of the operation here
                 $user_id = filter_var($user_id, FILTER_SANITIZE_NUMBER_INT);
                 if($id == $user_id){
-                    $sql = "SELECT first_name, last_name, email, phone, dob, joined FROM `users` WHERE id = $id";
+                    $sql = "SELECT id, first_name, last_name, email, phone, dob, joined FROM `users` WHERE id = $id";
                     $result = mysqli_query($db, $sql);
                     $rows = mysqli_fetch_array($result, MYSQLI_ASSOC);
 
@@ -46,11 +46,11 @@
                     http_response_code(401);
                     echo json_encode(array(
                         "message" => "Access denied. You can change your password only."
-                    )); 
+                    ));
                 }
-        
+
             }catch (Exception $e){
-        
+
                 http_response_code(401);
                 echo json_encode(array(
                     "message" => "Access denied.",
@@ -64,5 +64,5 @@
             "message" => "Access denied."
         ));
     }
- 
+
 ?>
